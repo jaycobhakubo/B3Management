7aeba825fa84aa0fdffe8000bd4c2e423bb2eefe
karlo <karlocamacho@gmail.com>
2018-01-26 09:13:38 -0800

    Added new SP

---
+++ B3Reports/REF/SQL/usp_management_report_WinningCardReport.sql
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTech.B3Reports.REF.SQL
+{
+	class usp_management_report_WinningCardReport
+	{
+	}
+}
